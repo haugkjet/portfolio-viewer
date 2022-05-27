e56313d68832569e98c91605115639cd247fc4aa
Kjetil Haugland <haugkjet@gmail.com>
2022-05-27 12:29:03 +0200

    Only draw at init

--- src/client/client.ts
+++ src/client/client.ts
@@ -13,7 +13,7 @@ const divisions = 20;
 const gridHelper = new THREE.GridHelper(size, divisions);
 scene.add(gridHelper);
 
-gridHelper.position.x = 10;
+//gridHelper.position.x = 10;
 
 scene.add(new THREE.AxesHelper(10));
 
@@ -40,48 +40,56 @@ const geometry = new THREE.BoxGeometry();
 
 const planegeometry = new THREE.PlaneGeometry();
 
-const geometry2 = new THREE.CylinderGeometry();
-
-const material = new THREE.MeshPhysicalMaterial({});
-material.reflectivity = 1.0;
-material.transmission = 0.0;
-material.roughness = 0.5;
-material.metalness = 0;
-material.clearcoat = 0.0;
-material.clearcoatRoughness = 0.0;
-material.color = new THREE.Color(0x0000ff);
-material.ior = 1.2;
-material.thickness = 10.0;
-
-const material1 = new THREE.MeshStandardMaterial({
+const materialgreen = new THREE.MeshStandardMaterial({
   color: 0x00ff00,
   wireframe: false,
 });
 
-const material2 = new THREE.MeshStandardMaterial({
+const materialred = new THREE.MeshStandardMaterial({
   color: 0xff0000,
   wireframe: false,
 });
 
-const material3 = new THREE.MeshStandardMaterial({
-  color: 0x0000ff,
+const materialgray = new THREE.MeshStandardMaterial({
+  color: 0x404040,
   wireframe: false,
 });
 
-const cube1 = new THREE.Mesh(geometry, material1);
+const cube1 = new THREE.Mesh(geometry, materialgreen);
 scene.add(cube1);
 
-const cube2 = new THREE.Mesh(geometry, material2);
+const cube2 = new THREE.Mesh(geometry, materialred);
 scene.add(cube2);
 
-const cube3 = new THREE.Mesh(geometry, material3);
+const cube3 = new THREE.Mesh(geometry, materialgreen);
 scene.add(cube3);
 
-const cyl1 = new THREE.Mesh(geometry2, material);
-scene.add(cyl1);
-
-const plane = new THREE.Mesh(planegeometry, material);
+const plane = new THREE.Mesh(planegeometry, materialgray);
 scene.add(plane);
+
+plane.scale.x = 20;
+plane.scale.y = 20;
+plane.scale.z = 20;
+
+plane.position.y = -0.05;
+
+plane.rotation.x = -1.5707;
+
+cube1.position.x = 0.5;
+cube1.position.y = 0.5;
+cube1.position.z = 0.5;
+
+cube2.position.x = 1.7;
+cube2.position.y = 1.0;
+cube2.position.z = 0.5;
+
+cube2.scale.y = 2;
+
+cube3.position.x = 2.9;
+cube3.position.y = 2;
+cube3.position.z = 0.5;
+
+cube3.scale.y = 4;
 
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
@@ -97,34 +105,10 @@ document.body.appendChild(stats.dom);
 function animate() {
   requestAnimationFrame(animate);
 
-  plane.scale.x = 0.1;
-  plane.scale.y = 0.1;
-  plane.scale.z = 0.1;
-
-  //plane.rotation.x = -1.5707;
-
-  cube1.position.x = 0.5;
-  cube1.position.y = 0.5;
-  cube1.position.z = 0.5;
-
-  cube2.position.x = 1.7;
-  cube2.position.y = 1.0;
-  cube2.position.z = 0.5;
-
-  cube2.scale.y = 2;
-
-  cube3.position.x = 2.9;
-  cube3.position.y = 2;
-  cube3.position.z = 0.5;
-
-  cube3.scale.y = 4;
-
-  cyl1.position.x = 5;
-  cyl1.position.y = 2.5;
-  cyl1.scale.y = 5;
-
-  //cube.rotation.x += 0.01;
-  //cube.rotation.y += 0.01;
+  //cube1.rotation.x += 0.01;
+  //cube1.rotation.y += 0.01;
+  //cube2.rotation.y += 0.01;
+  //cube3.rotation.y += 0.01;
 
   stats.update();
 
