ff35f54503cba2e017ba81b6a4c25ced7c05f326
Kjetil H <haugkjet@gmail.com>
2022-06-05 20:17:32 +0200

    Changing text and font size based on lod, mockup

--- src/client/font3d.ts
+++ src/client/font3d.ts
@@ -3,28 +3,50 @@ import * as THREE from "three";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 
+const lod = new THREE.LOD();
+
+//Create spheres with 3 levels of detail and create new LOD levels for them
+
 function initfont3d(scene: THREE.Scene) {
   const loader = new FontLoader();
+  const words = ["aaa", "bbbbbbbbbbbbb", "ccccc", "ddd", "e"];
+  for (let i = 0; i < 5; i++) {
+    loader.load("helvetiker_regular.typeface.json", function (font) {
+      const geometry2 = new TextGeometry(words[i], {
+        font: font,
+        size: 0.3 * i,
+        height: 0.01,
+        curveSegments: 12,
+        bevelEnabled: false,
+      });
+      const textMaterial = new THREE.MeshStandardMaterial({
+        //color: 0x000000, //Black
+        color: 0xffffff,
+      }); //Light });
 
-  loader.load("helvetiker_regular.typeface.json", function (font) {
-    const geometry = new TextGeometry("Portfolio", {
-      font: font,
-      size: 0.8,
-      height: 0.01,
-      curveSegments: 12,
-      bevelEnabled: false,
+      var mesh = new THREE.Mesh(geometry2, textMaterial);
+      mesh.position.set(-2, 1.0, 0.0);
+      mesh.rotation.set(-1.57, 0, 0);
+
+      const material = new THREE.MeshPhongMaterial({ color: 0xabcde });
+
+      const geometry = new THREE.BoxGeometry(5 - i, 2, 2);
+
+      // const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+      const cube = new THREE.Mesh(geometry, material);
+
+      cube.position.x = -5;
+
+      cube.add(mesh);
+
+      lod.addLevel(cube, i * 5);
+
+      scene.add(lod);
+
+      //scene.add(mesh);
     });
-    const textMaterial = new THREE.MeshStandardMaterial({
-      //color: 0x000000, //Black
-      color: 0xffffff,
-    }); //Light });
-
-    var mesh = new THREE.Mesh(geometry, textMaterial);
-    mesh.position.set(-2, 0.01, 2.0);
-    mesh.rotation.set(-1.57, 0, 0);
-
-    scene.add(mesh);
-  });
+  }
 
   return true;
 }
